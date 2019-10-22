e2ef7dc17ef7cdc72c52492a186c6d0a956cdb31
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2019-10-22 12:11:57 +0200

    Updates

---
+++ precache-manifest.79b63332d6bd9179367a7873973f6e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a1eac1624e67b87883bcd8729bf408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48baa4c7e2a99089d496",
+    "url": "/static/css/main.bc56c303.chunk.css"
+  },
+  {
+    "revision": "18b447c28c5ee8263ac8",
+    "url": "/static/js/2.0405dd18.chunk.js"
+  },
+  {
+    "revision": "48baa4c7e2a99089d496",
+    "url": "/static/js/main.95d62364.chunk.js"
+  },
+  {
+    "revision": "5e80ca3339c45b2a7ec1",
+    "url": "/static/js/runtime-main.95d3b8a1.js"
+  }
+]);
