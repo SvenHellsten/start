d94e04476c7ad457aaf75c99e904bd4fcd372571
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2019-10-24 19:00:43 +0200

    needed a push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+  render() {
+    return (
+      <body>
+
+    <main>
+
+        <h1>Sven<br />Hellsten</h1>
+
+        <p>I am a soon to be graduated Computer Science student from <a target="_blank" rel="noopener noreferrer" href="https://www.chalmers.se/en/education/programmes/masters-info/Pages/Computer-Science-algorithms-languages-and-logic.aspx">Chalmers</a>. I have a true love for problem solving and writing code. I specificly enjoy Computer Graphics and aucustics. Outside of tech I like cooking and running. If you are looking for a fullstack developer, someone to go for a run with, or want to grab your tin foil hat and have a chat about the issues with todays technology. Send me an email.</p>
+
+        <ul class="links">
+            <li><a href="mailto:sven.hellsten@gmail.com">Email</a></li>
+        </ul>
+
+    </main>
+
+    <script async src="javascript/ga.js"></script>
+
+</body>
+        
+    );
+  }
 }
 
 export default App;
