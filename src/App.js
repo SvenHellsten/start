29b2861cd9ddbb6dece0ff2439a99a2ba6842fd3
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2020-05-04 12:03:42 +0200

    soon to be to recently

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component  {
 
         <h1>Sven<br />Hellsten</h1>
 
-        <p>I am a soon to be graduated Computer Science student from <a target="_blank" rel="noopener noreferrer" href="https://www.chalmers.se/en/education/programmes/masters-info/Pages/Computer-Science-algorithms-languages-and-logic.aspx">Chalmers</a>. I have a true love for problem solving and writing code. I specificly enjoy Computer Graphics and aucustics. Outside of tech I like cooking and running. If you are looking for a fullstack developer, someone to go for a run with, or want to grab your tin foil hat and have a chat about the issues with todays technology. Send me an email.</p>
+        <p>I am recently graduated Computer Science student from <a target="_blank" rel="noopener noreferrer" href="https://www.chalmers.se/en/education/programmes/masters-info/Pages/Computer-Science-algorithms-languages-and-logic.aspx">Chalmers</a>. I have a true love for problem solving and writing code. I specificly enjoy Computer Graphics and acoustics. Outside of tech I like cooking and running. If you are looking for a fullstack developer, someone to go for a run with, or want to grab your tin foil hat and have a chat about the issues with todays technology. Send me an email.</p>
 
         <ul class="links">
             <li><a href="mailto:sven.hellsten@gmail.com">Email</a></li>
